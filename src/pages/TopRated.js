e33b4da10d8d11a510ce8c651299da35c5e43b7c
Sara Carlstein <carlstein.sara@gmail.com>
2021-05-06 15:52:29 +0200

    new stuff

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
+import { Grid, Card } from "@material-ui/core";
+import Searches from "../components/Searches";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "30px",
+  },
+  card: {
+    width: "140px",
+    margin: "10px",
+    padding: "10px",
+  },
+});
+
+// The Effect Hook lets you perform side effects in function components:
 export const TopRated = () => {
-  return <div>hej</div>;
+  const classes = useStyles();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://saras-books.herokuapp.com/books/top-rated")
+      .then((res) => res.json())
+      .then((json) => {
+        setBooks(json);
+      });
+  }, []);
+
+  return (
+    <>
+      <Grid container className={classes.root} spacing={1}>
+        {books.map((book) => (
+          <div className="book-card" key={book.bookID}>
+            <Card className={classes.card}>
+              <Link to={`/books/book/${book.bookID}`}>
+                <h3>{book.title}</h3>
+                <p>{book.authors}</p>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </Grid>
+    </>
+  );
 };
