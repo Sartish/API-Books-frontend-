acbb0ec8052597d59f82457ee377ce63d16ab3ac
Sara Carlstein <carlstein.sara@gmail.com>
2021-06-24 17:26:52 +0200

    redeployed database

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { CardActionArea, Typography, Grid, makeStyles, useTheme, useMediaQuery} from "@material-ui/core";
-import front from "../assets/Aretha-front.jpeg"
-import header from "../assets/small.png"
-import banner from "../assets/gold.png"
-import bannerLarge from "../assets/banner.lg.png"
+import {
+  CardActionArea,
+  Typography,
+  Grid,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+} from "@material-ui/core";
+import front from "../assets/Aretha-front.jpeg";
+import header from "../assets/small.png";
+import banner from "../assets/gold.png";
+import bannerLarge from "../assets/banner.lg.png";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   grid: {
-    backgroundColor:  '#a3d2ca',
-    backgroundImage: 'none',
+    backgroundColor: "#a3d2ca",
+    backgroundImage: "none",
     backgroundSize: "center",
     backgroundRepeat: "no-repeat",
-    [theme.breakpoints.up('sm')]: {
-      backgroundColor: 'none',
+    [theme.breakpoints.up("sm")]: {
+      backgroundColor: "none",
     },
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       backgroundImage: `url(${banner})`,
       backgroundRepeat: "none",
     },
-    [theme.breakpoints.up('lx')]: {
+    [theme.breakpoints.up("lx")]: {
       backgroundImage: `url(${bannerLarge})`,
       backgroundRepeat: "none",
     },
-
   },
   top: {
     backgroundImage: `url(${front})`,
@@ -34,10 +40,9 @@ const useStyles = makeStyles(theme => ({
     // [theme.breakpoints.up('sm')]: {
     //   with: '200px',
     // },
-    [theme.breakpoints.up('md')]: {
-      backgroundImage: 'none',
+    [theme.breakpoints.up("md")]: {
+      backgroundImage: "none",
       height: "580px",
-      
     },
   },
   bottom: {
@@ -49,18 +54,17 @@ const useStyles = makeStyles(theme => ({
     // [theme.breakpoints.up('sm')]: {
     //   with: '300px',
     // },
-    [theme.breakpoints.up('md')]: {
-      backgroundImage: 'none',
+    [theme.breakpoints.up("md")]: {
+      backgroundImage: "none",
     },
   },
 
-    // header: {
-    //   height: "400px",
-    //   width: "300px",
-    //   marginLeft: "20px",
-    // },
-
-}))
+  // header: {
+  //   height: "400px",
+  //   width: "300px",
+  //   marginLeft: "20px",
+  // },
+}));
 
 export const FrontPage = () => {
   const classes = useStyles();
@@ -68,28 +72,30 @@ export const FrontPage = () => {
   // const showText = useMediaQuery(theme.breakpoints.up('sm'));
 
   return (
-    <Grid className={classes.grid}
-        container
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-      >
-       {/* {showText && <Typography variant="h1">Appear when 600px and above</Typography>} */}
-    <CardActionArea className={classes.top}>
-       <Typography
-      className={classes.header}
-      gutterBottom variant="h5" 
-      component="h2">
-      </Typography> 
-      </CardActionArea> 
+    <Grid
+      className={classes.grid}
+      container
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      {/* {showText && <Typography variant="h1">Appear when 600px and above</Typography>} */}
+      <CardActionArea className={classes.top}>
+        <Typography
+          className={classes.header}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        ></Typography>
+      </CardActionArea>
       <CardActionArea className={classes.bottom}>
-       <Typography
-      className={classes.header}
-      gutterBottom variant="h5" 
-      component="h2">
-      </Typography> 
-      </CardActionArea> 
-      </Grid>
-  
+        <Typography
+          className={classes.header}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        ></Typography>
+      </CardActionArea>
+    </Grid>
   );
 };
